5a5c1a284a9ae3474462d157b1bb00fa9cc09c0c
Nurmira Zhorobek kyzy <97220234+Nurmira16@users.noreply.github.com>
2023-06-05 11:28:02 +0200

    feat: add eventListener

--- main.js
+++ main.js
@@ -2,4 +2,15 @@ window.addEventListener('load', () => {
     const form = document.querySelector('#new-task-form')
     const input = document.querySelector('#new-task-input')
     const list_el = document.querySelector('#tasks')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const task = input.value
+        if (!task) {
+            alert('please fill out the task')
+        }
+        task_delete_el.addEventListener('click', () => {
+            list_el.removeChild(task_el)
+        })
+    })
 })
